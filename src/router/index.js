34700bb1998f05f022442f7f7e6f81fcfd878d1d
Filiup <filip.barabas@student.spseke.sk>
2022-01-02 16:58:09 +0100

    pridanie home (default) routy

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,15 @@
-/*
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
+import Home from '../views/Home';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import( webpackChunkName: "about"  "../views/About.vue"),
-  },
-];
 
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -26,4 +17,3 @@ const router = createRouter({
 });
 
 export default router;
-*/
