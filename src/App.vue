e564518a142ea3d72b0eab4af645f31e7358b8e5
Filiup <filip.barabas@student.spseke.sk>
2022-01-02 16:57:38 +0100

    pridanie router-view pre spravnu funkcnost routera

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,5 @@
-<!--
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
--->
-<template>
-  <div>
 
-  </div>
-</template>
 
-<script>
-import { reactive, toRefs } from 'vue'
-
-export default {
-  setup () {
-    const state = reactive({
-      count: 0,
-    })
-  
-    return {
-      ...toRefs(state),
-    }
-  }
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
