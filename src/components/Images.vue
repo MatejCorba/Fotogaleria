2b1de219e7a6ba7ada0290bd28590cec69e054f2
Filiup <filip.barabas@student.spseke.sk>
2022-01-02 17:56:25 +0100

    vykreslovanie obrazkov cez v-for cyklus

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,48 +1,34 @@
 <template>
-    <div class="gallery gallery-hover-efect">
-        <img class="img-gallery" src="obrazky/car.jpg" alt="gallery_name" title="name">
-        <p class="gallery-name" >Auta</p>
-    </div>
-
-
-    <div class="gallery gallery-hover-efect">
-        <img class="img-gallery" src="obrazky/house.jpg" alt="gallery_name" title="name">
-        <p class="gall ery-name" >Dom</p>
-    </div>
-
-    <div class="gallery gallery-hover-efect">
-        <img class="img-gallery" src="obrazky/food.jpg" alt="gallery_name" title="name">
-        <p class="gallery-name" >Jedlo</p>
-    </div>
-
-    <div class="gallery gallery-hover-efect">
-        <img class="img-gallery" src="obrazky/phone.jpg" alt="gallery_name" title="name">
-        <p class="gallery-name">Mobil</p>
-    </div>
-
-    <div class="gallery gallery-hover-efect">
-        <img class="img-gallery" src="obrazky/back1.jpg" alt="gallery_name" title="name">
-        <p class="gallery-name">Foto</p>
-    </div>
+  <div
+    v-for="image in $store.getters.getImages"
+    :key="image.id"
+    class="gallery gallery-hover-efect"
+  >
+    <img
+      class="img-gallery"
+      :src="image.path"
+      :alt="image.name"
+      :title="image.name"
+    />
+    <p class="gallery-name">{{ image.name }}</p>
+  </div>
 
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from 'vue';
 
 export default {
-    setup () {
-        const state = reactive({
-            count: 0,
-        })
-    
-        return {
-            ...toRefs(state),
-        }
-    }
-}
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
