0c1f05fb984492d86cd98a063a9e5f0155eb35f8
Filiup <filip.barabas@student.spseke.sk>
2022-01-02 18:50:01 +0100

    pridanie metod na ziskavanie udajov z API

--- src/store/images.js
+++ src/store/images.js
@@ -1,42 +1,28 @@
+import axios from 'axios';
+
 export const images = {
   state: () => ({
-    galleries: [
-      {
-        id: 1,
-        path: 'obrazky/car.jpg',
-        name: 'Auta',
-      },
-      {
-        id: 2,
-        path: 'obrazky/house.jpg',
-        name: 'Dom',
-      },
-
-      {
-        id: 3,
-        path: 'obrazky/food.jpg',
-        name: 'Jedlo',
-      },
-
-      {
-        id: 4,
-        path: 'obrazky/phone.jpg',
-        name: 'Mobil',
-      },
-
-      {
-        id: 5,
-        path: 'obrazky/back1.jpg',
-        name: 'Foto',
-      },
-    ],
+    galleries: [],
   }),
 
-  mutations: {},
-  actions: {},
+  mutations: {
+    getGalleriesFromAPI(state, galleries) {
+        state.galleries = galleries
+    }
+  },
+
+  actions: {
+    async getGalleriesFromAPI({ commit }) {
+      const response = await axios.get('http://localhost:3000/api/galleries');
+      commit("getGalleriesFromAPI", response.data);
+    },
+  },
   getters: {
     getImages(state) {
       return state.galleries;
     },
   },
 };
+
+
+
