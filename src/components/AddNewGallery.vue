e0a0f6b503df0e9ba26ebdf400ac8615a38c687a
MatejCorba <matej.corba@student.spseke.sk>
2022-01-04 11:27:15 +0100

    pridanie metody na zobrazenie modal komponentu

--- src/components/AddNewGallery.vue
+++ src/components/AddNewGallery.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="plus">
+  <div class="plus" @click="plusIconClicked">
     <i class="fas fa-plus"></i>
   </div>
 </template>
 
 <script>
 import { reactive, toRefs } from 'vue';
+import { useStore } from "vuex";
 
 export default {
   setup() {
+    const store = useStore();
     const state = reactive({
       count: 0,
     });
 
+    const plusIconClicked = () => {
+      store.dispatch(`galleries/changeModalVisibility`)
+    }
+
+
     return {
       ...toRefs(state),
+      plusIconClicked
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  .plus {
+    cursor: pointer;
+  }
+</style>
