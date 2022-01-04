34b6649591e1cd5bd26c6a6055db1e93dec6ce50
MatejCorba <matej.corba@student.spseke.sk>
2022-01-04 11:29:36 +0100

    vycentrovanie modalu do stredu obrazovky

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 // css
-import "./css/style.css";
+import './css/style.css';
 
-createApp(App).use(store).use(router).mount("#app");
+
+createApp(App).use(store).use(router).mount('#app');
