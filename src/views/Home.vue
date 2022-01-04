94fce45c38f7137cf53f2e51e732ea0dc5d08190
Filiup <filip.barabas@student.spseke.sk>
2022-01-04 20:41:04 +0100

    Premenovanie komponentu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 
   <div class="grid-container">
     <Galleries />
-    <AddNewGallery />
+    <NewGalleryIcon />
   </div>
   <NewGalleryModal v-if="$store.state.galleries.newGalleryClicked" />
 
@@ -14,14 +14,14 @@
 import { reactive, toRefs, onMounted } from 'vue';
 import { useStore } from 'vuex';
 import Galleries from '../components/Galleries.vue';
-import AddNewGallery from '../components/AddNewGallery.vue';
+import NewGalleryIcon from '../components/NewGalleryIcon.vue';
 import Header from '../components/Header.vue';
 import NewGalleryModal from '../components/NewGalleryModal.vue';
 
 export default {
   components: {
     Galleries,
-    AddNewGallery,
+    NewGalleryIcon,
     Header,
     NewGalleryModal,
   },
