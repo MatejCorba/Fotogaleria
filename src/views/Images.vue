279429454ec5f6455155061fe867104b4690a936
MatejCorba <matej.corba@student.spseke.sk>
2022-01-05 10:59:40 +0100

    Vytvorenie computed prop pre router parametre

--- src/views/Images.vue
+++ src/views/Images.vue
@@ -1,22 +1,28 @@
 <template>
-  <Header :title="$route.params.GalleryName.toUpperCase()" />
+  <Header :title="galleryName.toUpperCase()" />
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import { reactive, toRefs, computed } from 'vue';
 import Header from '../components/Header.vue';
+import { useRoute } from 'vue-router';
 
 export default {
   components: {
     Header,
   },
   setup() {
+    const router = useRoute();
     const state = reactive({
       count: 0,
     });
 
+    // Computed properties
+    const galleryName = computed(() => router.params.galleryName);
+
     return {
       ...toRefs(state),
+      galleryName,
     };
   },
 };
