e512302b5b3fd110b4e3cc41f4b6caddd9dc3131
Filiup <filip.barabas@student.spseke.sk>
2022-01-05 19:28:17 +0100

    "cursor: pointer" pre "plus" ikonku

--- src/components/images/NewImageIcon.vue
+++ src/components/images/NewImageIcon.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="plus-images">
-      <i class="fas fa-plus"></i>
-    </div>
+  <div class="plus-images">
+    <i class="fas fa-plus"></i>
+  </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from 'vue';
 
 export default {
-    setup () {
-        const state = reactive({
-            count: 0,
-        })
-    
-        return {
-            ...toRefs(state),
-        }
-    }
-}
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.plus-images {
+  cursor: pointer;
+}
 </style>
