6ac6f5553a20499b9d85a372693a0d708100ae71
MatejCorba <matej.corba@student.spseke.sk>
2022-01-08 13:44:57 +0100

    Pridanie eventov ktore zobrazia/skryju modal

--- src/components/images/AddImagesModal.vue
+++ src/components/images/AddImagesModal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="overlay overlay-on"></div>
+  <div class="overlay overlay-on" @click="changeModalVisibility"></div>
   <section class="center-add-img add-image-modal">
-    <i class="fas fa-times close"></i>
+    <i class="fas fa-times close" @click="changeModalVisibility"></i>
     <h1 class="text-addImg">PRIDAJ FOTKY</h1>
     <div class="drop-images">
       <i class="fas fa-camera"></i>
@@ -17,16 +17,23 @@
 
 <script>
 import { reactive, toRefs } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
   setup() {
+    const store = useStore();
     const state = reactive({
       count: 0,
     });
 
+    const changeModalVisibility = () =>
+      store.dispatch('images/changeModalVisibility');
+
     return {
       ...toRefs(state),
+      changeModalVisibility
     };
+
   },
 };
 </script>
--- src/components/images/NewImageIcon.vue
+++ src/components/images/NewImageIcon.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="plus-images">
-    <i class="fas fa-plus"></i>
+    <i class="fas fa-plus" @click="changeModalVisibility"></i>
   </div>
 </template>
 
 <script>
 import { reactive, toRefs } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
   setup() {
+    const store = useStore();
     const state = reactive({
       count: 0,
     });
 
+    const changeModalVisibility = () => store.dispatch(
+      'images/changeModalVisibility'
+    );
+
     return {
       ...toRefs(state),
+      changeModalVisibility
     };
   },
 };
