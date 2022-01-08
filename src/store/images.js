2a3d90dc1962cedb98722116386b262eb63b91d9
MatejCorba <matej.corba@student.spseke.sk>
2022-01-08 13:15:41 +0100

    pridanie metody na zobrazenie/skrytie modalu

--- src/store/images.js
+++ src/store/images.js
@@ -6,16 +6,23 @@ export const images = {
 
   state: () => ({
     images: [],
+    showImageModal: false,
   }),
   mutations: {
     getImagesFromAPI(state, galleryImages) {
       state.images = galleryImages;
+    },
+    changeModalVisibility(state) {
+      state.showImageModal = !state.showImageModal;
     },
   },
   actions: {
     async getImagesFromAPI({ commit }, galleryName) {
       const response = await axios.get(config.API_IMAGES_URI(galleryName));
       commit('getImagesFromAPI', response.data);
+    },
+    changeModalVisibility({ commit }) {
+      commit('changeModalVisibility');
     },
   },
   getters: {},
