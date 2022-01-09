9d0d8e9bb58d463819049926b555d0ee3978c3ac
MatejCorba <matej.corba@student.spseke.sk>
2022-01-09 10:47:22 +0100

    Pridanie event listenerov ktore pocuvaju na drag&drop

--- src/components/images/addImagesModal/DragDrop.vue
+++ src/components/images/addImagesModal/DragDrop.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="drop-images">
+  <div
+    class="drop-images"
+    :class="{ 'drop-border-solid': borderSolid }"
+    @dragover.prevent="changeBorderSolid"
+    @dragend.prevent="changeBorderDotted"
+    @dragleave.prevent="changeBorderDotted"
+    @drop.prevent="imageUpload"
+  >
     <i class="fas fa-camera"></i>
     <p class="text-center">
       SEM PRESUNTE FOTKY <br />
@@ -12,6 +19,7 @@
 <script>
 import { reactive, toRefs } from 'vue';
 import UploadButton from './UploadButton.vue';
+import { useStore } from 'vuex';
 
 export default {
   props: {
@@ -22,13 +30,30 @@ export default {
   components: {
     UploadButton,
   },
-  setup() {
+  setup(props) {
+    const store = useStore();
     const state = reactive({
-      count: 0,
+      borderSolid: false,
     });
+
+    const changeBorderSolid = () => {
+      state.borderSolid = true;
+    };
+    const changeBorderDotted = () => {
+      state.borderSolid = false;
+    };
+    const imageUpload = (files) => {
+      store.dispatch('images/uploadImagesToAPI', {
+        fileArray: [...files.dataTransfer.files],
+        galleryName: props.galleryName,
+      });
+    };
 
     return {
       ...toRefs(state),
+      changeBorderSolid,
+      changeBorderDotted,
+      imageUpload,
     };
   },
 };
