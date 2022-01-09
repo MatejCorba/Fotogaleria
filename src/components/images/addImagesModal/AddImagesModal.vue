d96f5bb575ec5212a5e4f554089a3870389a8358
MatejCorba <matej.corba@student.spseke.sk>
2022-01-09 11:45:14 +0100

    nahravanie obrazkov pomocou tlačidla Pridat

--- src/components/images/addImagesModal/AddImagesModal.vue
+++ src/components/images/addImagesModal/AddImagesModal.vue
@@ -3,8 +3,10 @@
   <section class="center-add-img add-image-modal">
     <i class="fas fa-times close" @click="changeModalVisibility"></i>
     <h1 class="text-addImg">PRIDAJ FOTKY</h1>
-    <DragDrop :galleryName="galleryName" />
-    <button class="img-btn"><i class="fas fa-plus"></i> Pridať</button>
+    <DragDrop :galleryName="galleryName" @uploadReady="enableUploadButton" />
+    <button class="img-btn" @click="uploadImagesToAPI">
+      <i class="fas fa-plus"></i> Pridať
+    </button>
   </section>
 </template>
 
@@ -25,15 +27,30 @@ export default {
   setup() {
     const store = useStore();
     const state = reactive({
-      count: 0,
+      buttonUpload: false,
+      filesToUpload: {},
     });
 
     const changeModalVisibility = () =>
       store.dispatch('images/changeModalVisibility');
 
+    const enableUploadButton = (files) => {
+      state.buttonUpload = true;
+      state.filesToUpload = files;
+    };
+
+    const uploadImagesToAPI = () => {
+      if (!state.buttonUpload) return;
+
+      store.dispatch('images/uploadImagesToAPI', state.filesToUpload);
+      store.dispatch('images/changeModalVisibility');
+    };
+
     return {
       ...toRefs(state),
       changeModalVisibility,
+      enableUploadButton,
+      uploadImagesToAPI,
     };
   },
 };
