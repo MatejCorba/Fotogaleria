af1855936d5f522eb70c656eac07e006f55c1edc
Filiup <filip.barabas@student.spseke.sk>
2022-01-10 20:58:44 +0100

    Zmena rozlisenia nahladovych obrazkov na 300x200px

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   preview_width: '300',
-  preview_height: '300',
+  preview_height: '200',
   API_GALLERIES_URI: (id = false, preview = false) =>
     `http://localhost:3000/api/galleries${id ? '/' + id : ``}` +
     (preview
