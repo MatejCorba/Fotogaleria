3413d6f9d68304337f0dc29276c3a5012656a149
Filiup <filip.barabas@student.spseke.sk>
2022-01-11 20:36:50 +0100

    Zjednoduhsienie config suboru, pridanie moznosti "runInDocker"

--- src/config.js
+++ src/config.js
@@ -1,24 +1,25 @@
+const runInDocker = false;
+const preview_width = '300';
+const preview_height = '';
+
+const toQueryString = (width, height) => {
+  const previewSize = {};
+  if (width) previewSize.width = width;
+  if (height) previewSize.height = height;
+
+  return '?' + new URLSearchParams(previewSize).toString();
+};
+
 export const config = {
-  preview_width: '300',
-  preview_height: '200',
+  base_url: runInDocker ? 'fotogaleria' : 'localhost:3000',
+  previewSizeQuery: toQueryString(preview_width, preview_height),
+
   API_GALLERIES_URI: (id = '', preview = false) =>
-    `http://localhost:3000/api/galleries${id ? '/' + id : ``}` +
-    (preview
-      ? config.preview_width && config.preview_height
-        ? `?width=${config.preview_width}&height=${config.preview_height}`
-        : config.preview_width
-        ? `?width=${config.preview_width}`
-        : `?height=${config.preview_height}`
-      : ``),
+    `http://${config.base_url}/api/galleries${id ? '/' + id : ``}` +
+    (preview ? config.previewSizeQuery : ``),
+
   API_IMAGES_URI: (galleryName, id = '', preview = false) =>
-    `http://localhost:3000/api/galleries/images/${galleryName}${
+    `http://${config.base_url}/api/galleries/images/${galleryName}${
       id ? '/' + id : ``
-    }` +
-    (preview
-      ? config.preview_width && config.preview_height
-        ? `?width=${config.preview_width}&height=${config.preview_height}`
-        : config.preview_width
-        ? `?width=${config.preview_width}`
-        : `?height=${config.preview_height}`
-      : ``),
+    }` + (preview ? config.previewSizeQuery : ``),
 };
