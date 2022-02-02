9e237e92966478e7b2bc6e9eb770d78d97b109ea
Filiup <filip.barabas@student.spseke.sk>
2022-02-02 12:21:08 +0100

    Pridanie dokumentácie do "config.js"

--- src/config/config.js
+++ src/config/config.js
@@ -15,6 +15,14 @@ const config = {
   previewSizeQuery: toQueryString(PREVIEW_WIDTH, PREVIEW_HEIGHT),
   exifQuery: '?image=false',
 
+  /**
+   * Generácia URI na získavanie údajov ohľadom galérii z API
+   * @param {Object} URI - Do funkcie treba poslať objekt obsahúci údaje ohľadom URI
+   * @param {string=} URI.id - Id galérie, ktorú cheme zobraziť
+   * @param {Boolean=} URI.preview - Po nastavení tejto hodnoty na true sa zmenia rezmery náhľ. obrázka na rozmery v "config.json"
+   * @param {Boolean=} URI.exif - Po nastavení tejto hodnoty na true sa nam namiesto náhľ. obrázka zobrazi jeho exif
+   */
+
   API_GALLERIES_URI: ({ id, preview, exif }) =>
     `${API_BASE_URL}/api/galleries${id ? '/' + id : ``}` +
     (id && preview
@@ -22,6 +30,15 @@ const config = {
       : id && exif
       ? config.exifQuery
       : ``),
+
+  /**
+   * Generácia URI na získavanie údajov ohľadom obrázkov z API
+   * @param {Object} URI - Do funkcie treba poslať objekt obsahúci údaje ohľadom URI
+   * @param {String} URI.name - Meno galérie, z ktorej chceme zobraziť obrázky
+   * @param {string=} URI.id - Id obrázka, ktorý chceme zobraziť
+   * @param {Boolean=} URI.preview - Po nastavení tejto hodnoty na true sa zmenia rezmery obrázka na rozmery v "config.json"
+   * @param {Boolean=} URI.exif - Po nastavení tejto hodnoty na true sa nam namiesto obrázka zobrazi jeho exif
+   */
 
   API_IMAGES_URI: ({ gallery, id, preview, exif }) => {
     if (!gallery) throw new Error('Neposlal si meno galérie.');
