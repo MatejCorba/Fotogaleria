370a9eef9d05de041157e8f356c996040439dc58
MatejCorba <matej.corba@student.spseke.sk>
2022-02-11 20:19:13 +0100

    pridanie google gps do exif modalu

--- src/components/images/ExifModal.vue
+++ src/components/images/ExifModal.vue
@@ -9,6 +9,15 @@
         <span v-for="(exif, index) in Object.keys(exifData)" :key="index">
           {{ exif }}: {{ exifData[exif] }} <br />
         </span>
+        <iframe
+        width = 400px
+        class="google-gps"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ><a href="https://www.gps.ie/golf-gps/">buy golf golf</a></iframe>
       </p>
     </div>
   </section>
@@ -42,4 +51,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.google-gps{
+  padding-top: 20px;
+}
+</style>
